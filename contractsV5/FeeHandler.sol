7a0d69f35066da497b3cf1dadadcb1fe1b884c08
Simon Tan <simon.tan.yu.jing@gmail.com>
2020-01-14 17:06:11 +0800

    added some pseudocode comments, updated variable naming for encode / decode

--- contractsV5/FeeHandler.sol
+++ contractsV5/FeeHandler.sol
@@ -2,8 +2,9 @@ pragma solidity 0.5.11;
 
 import "./IKyberDAO.sol";
 import "./IFeeHandler.sol";
+import "./UtilsV5.sol";
 
-contract FeeHandler is IFeeHandler {
+contract FeeHandler is IFeeHandler, UtilsV5 {
 
     IKyberDAO public kyberDAOContract;
 
@@ -13,8 +14,7 @@ contract FeeHandler is IFeeHandler {
 
     uint public brrAndEpochData;
 
-    uint constant SEPARATOR_BITS = 64;
-    uint constant MAX_BPS = 10000;
+    uint constant BITS_PER_PARAM = 64;
     uint public brrAndEpochData;
     uint public burnInBPS;
     uint public rebateInBPS;
@@ -31,26 +31,35 @@ contract FeeHandler is IFeeHandler {
 
         
     function encodeData(uint _burn, uint _reward, uint _epoch, uint _expiryBlock) public {
-        // return leftShift(leftShift(leftShift(a, 2) + b, 2) + c, 2) + d;
-        brrAndEpochData = (((((_burn << SEPARATOR_BITS) + _reward) << SEPARATOR_BITS) + _epoch) << SEPARATOR_BITS) + _expiryBlock;
+        brrAndEpochData = (((((_burn << BITS_PER_PARAM) + _reward) << BITS_PER_PARAM) + _epoch) << BITS_PER_PARAM) + _expiryBlock;
     }
-    
+    // todo: return 5 values from decode data, remove storage variables
     function decodeData() public {
-        expiryBlock = brrAndEpochData & (1 << SEPARATOR_BITS) - 1;
-        epoch = (brrAndEpochData / (1 << SEPARATOR_BITS)) & (1 << SEPARATOR_BITS) - 1;
-        rewardInBPS = (brrAndEpochData / (1 << SEPARATOR_BITS << SEPARATOR_BITS)) & (1 << SEPARATOR_BITS) - 1;
-        burnInBPS = (brrAndEpochData / (1 << SEPARATOR_BITS << SEPARATOR_BITS << SEPARATOR_BITS)) & (1 << SEPARATOR_BITS) - 1;
-        rebateInBPS = MAX_BPS - rewardInBPS - burnInBPS;
+        expiryBlock = brrAndEpochData & (1 << BITS_PER_PARAM) - 1;
+        epoch = (brrAndEpochData / (1 << BITS_PER_PARAM)) & (1 << BITS_PER_PARAM) - 1;
+        rewardInBPS = (brrAndEpochData / (1 << BITS_PER_PARAM << BITS_PER_PARAM)) & (1 << BITS_PER_PARAM) - 1;
+        burnInBPS = (brrAndEpochData / (1 << BITS_PER_PARAM << BITS_PER_PARAM << BITS_PER_PARAM)) & (1 << BITS_PER_PARAM) - 1;
+        rebateInBPS = BPS - rewardInBPS - burnInBPS;
     }
 
     function handleFees(address[] calldata eligibleReserves, uint[] calldata rebatePercentages) external payable returns(bool) {
+        
+        // Per trade check epoch number, and if changed, call DAO to get existing percentage values for reward / burn / rebate
+        // Rebates to reserves if entitled. (if reserve isn’t entitled, it means fee wasn’t taken!)
+        // Internal accounting per reserve.
+        // Update total_reserve_rebate
+        // Update rewards
+        // Update total_reward [epoch]
+        // Update total_reward_amount.
+        // Eth for burning is the remaining == (total balance - total_reward_amount - total_reserve_rebate).
+
+
+        // When you update reserve rebate, you must first check if 2 reserves i.e. handled on both token to eth n eth to token.
+        // encode totals, 128 bits per reward / rebate.
+        // accumulate rebates per wallet instead of per reserve use reserveOwners
         return true;
     }
 
-    function setReserveAdmin(address reserve, address admin) public {
-        // Add onlyAdmin modifier
-
-    }
 
     function claimStakerReward(address staker, uint percentageinPrecision, uint epoch) public {
         // onlyDAO
@@ -68,13 +77,21 @@ contract FeeHandler is IFeeHandler {
         // if we include a dest address, we need an owner / admin of the reserve and the below function.
     }
 
+    function burnKNC() public {
+        // only DAO?
+        // convert fees to KNC and burn
+    }
+
+    // Don't work on this yet
+    function setReserveAdmin(address reserve, address admin) public {
+        // Add onlyAdmin modifier
+
+    }
+
     function updateReserveOwner(address owner) public {
         // only Admin?
         // do we really need this? think anyone can just call claimRebate for reserve and it'll just send the rebates to the reserve address;
     }
 
-    function burnKNC() public {
-        // only DAO?
-        // convert fees to KNC and burn
-    }
+
 }
